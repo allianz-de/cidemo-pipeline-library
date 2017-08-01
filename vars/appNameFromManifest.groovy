15e01919489e4170baa8bb9ef8ecc0aa86c6f21c
Julie Ng <me@julie.io>
2017-08-01 20:59:03 +0200

    feat: add helpers for deploying features

--- vars/appNameFromManifest.groovy
+++ vars/appNameFromManifest.groovy
@@ -1,5 +1,10 @@
 // vars/appNameFromManifest.groovy
-def call() {
-  def appInfo = readYaml file: './manifest.yml'
-  return appInfo.applications[0].name
+def call(Map params = [:]) {
+    def appInfo = readYaml file: './manifest.yml'
+    def baseName = appInfo.applications[0].name
+    if (params['append']) {
+        return baseName + '-' + params['append'].replace('/','-')
+    } else {
+        return baseName
+    }
 }
