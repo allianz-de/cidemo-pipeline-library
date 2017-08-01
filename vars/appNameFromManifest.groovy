fa244b651f82c8ab5371ace56d27c656d643c578
Julie Ng <me@julie.io>
2017-08-01 17:13:59 +0200

    more debugging

--- vars/appNameFromManifest.groovy
+++ vars/appNameFromManifest.groovy
@@ -1,15 +1,15 @@
 // vars/appNameFromManifest.groovy
 def call(boolean isFeature = false) {
-  def appInfo = readYaml file: './manifest.yml'
-  def baseName = appInfo.applications[0].name
+    def appInfo = readYaml file: './manifest.yml'
+    def baseName = appInfo.applications[0].name
 
-  println baseName
-
-  String result = isFeature
-    ? "${baseName}-${env.BRANCH_NAME.replace('/','-')}"
-    : baseName
-
-  println result
-
-  return baseName
+    if (isFeature) {
+        def branch = env.BRANCH_NAME.replace('/','-')
+        echo "baseName: ${baseName}"
+        echo "branch: ${branch}"
+        echo "together?"
+        def foo = "${baseName}-${branch}"
+        println foo
+    }
+    return baseName
 }
